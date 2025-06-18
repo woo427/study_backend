adc2404681378958677960f317188cba93e8957b
김지우 <annejwkim00@naver.com>
2025-06-18 22:58:32 +0900

    wip: spring security 구현 중

---
+++ NaverOauth/myselectshop/src/main/java/com/study/myselectshop/infrastructure/security/UserDetailsImpl.java
@@ -0,0 +1,66 @@
+package com.study.myselectshop.infrastructure.security;
+
+import com.study.myselectshop.domain.entity.user.User;
+import com.study.myselectshop.domain.entity.user.UserRoleEnum;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        // Spring Security가 이해할 수 있는 권한 객체로 변환.
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        // 한 사람의 권한이 여러 개일 수도 있으므로 list 생성.
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ NaverOauth/myselectshop/src/main/java/com/study/myselectshop/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,5 @@
+package com.study.myselectshop.infrastructure.security;
+
+public class UserDetailsServiceImpl {
+    
+}
