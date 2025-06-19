b94898c47ea4c6919eee9c14c919589e5f92de27
김지우 <annejwkim00@naver.com>
2025-06-19 18:02:33 +0900

    feat: userDetailService 구현

--- NaverOauth/myselectshop/src/main/java/com/study/myselectshop/infrastructure/security/UserDetailsServiceImpl.java
+++ NaverOauth/myselectshop/src/main/java/com/study/myselectshop/infrastructure/security/UserDetailsServiceImpl.java
@@ -1,5 +1,25 @@
 package com.study.myselectshop.infrastructure.security;
 
-public class UserDetailsServiceImpl {
+import com.study.myselectshop.domain.entity.user.User;
+import com.study.myselectshop.infrastructure.repository.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 아이디가 없음: " + username));
+
+        return new UserDetailsImpl(user);
+    }
     
 }
